a5628c8895df38ce8e2c2f1eb6acea3f82743b2f
Kendall lewis <kendall.ryan.lewis@gmail.com>
2024-03-13 22:00:49 -0500

    Initial Commit

---
+++ Cocktail-bar/Cocktail_barApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cocktail_barApp.swift
+//  Cocktail-bar
+//
+//  Created by Kendall Lewis on 3/13/24.
+//
+
+import SwiftUI
+
+@main
+struct Cocktail_barApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
