c03864e4d570ec6e1f266428d24f1387a45cee1c
Kendall lewis <kendall.ryan.lewis@gmail.com>
2024-04-20 18:42:37 -0500

    Fixing issue

---
+++ Cocktail-bar/ViewModel/WebView.swift
@@ -0,0 +1,18 @@
+//
+//  WebView.swift
+//  Cocktail-bar
+//
+//  Created by Kendall Lewis on 4/19/24.
+//
+
+import SwiftUI
+
+struct WebView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WebView()
+}
